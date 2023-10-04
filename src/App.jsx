fbd835de887f9ef484d935a701a0c8392fe2e732
Sakon Plangklang <iamsabell@gmail.com>
2023-10-05 01:52:00 +0700

    Finished the initial navbar design

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Navbar from "./comps/navbar/Navbar.comp";
 
 function App() {
   return (
-    <Container>
+    <>
       <Navbar />
       <h1>Hello, World</h1>
-    </Container>
+    </>
   );
 }
 
--- src/comps/navbar/Navbar.comp.jsx
+++ src/comps/navbar/Navbar.comp.jsx
@@ -1,27 +1,41 @@
 import React from "react";
 import "./Navbar.style.scss";
+import { AppBar, Container } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonIcon from "@mui/icons-material/Person";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 const Navbar = () => {
   return (
     <div className="navbar-container">
-      <div className="logo-container">
-        <h1>Logo</h1>
-      </div>
-      <div className="main-nav">
-        <ul>
-          <li>Home</li>
-          <li>Products</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </div>
-      <div className="sub-nav">
-        <ul>
-          <li>Search</li>
-          <li>Account</li>
-          <li>Cart</li>
-        </ul>
-      </div>
+      <Container>
+        <div className="navbar">
+          <div className="logo-container">
+            <h1>Logo</h1>
+          </div>
+          <div className="main-nav">
+            <ul>
+              <li>Home</li>
+              <li>Products</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+          <div className="sub-nav">
+            <ul>
+              <li>
+                <SearchIcon className="icon" />
+              </li>
+              <li>
+                <PersonIcon className="icon" />
+              </li>
+              <li>
+                <ShoppingCartIcon className="icon" />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
